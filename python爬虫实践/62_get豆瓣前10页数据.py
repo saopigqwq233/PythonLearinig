31200f2d4be7a970d66c941c800ddbc1a61b8b77
LiuWenhao <115407314+saopigqwq233@users.noreply.github.com>
2023-10-04 21:04:59 +0800

    python爬虫ajax

--- python爬虫实践/62_get豆瓣前10页数据.py
+++ python爬虫实践/62_get豆瓣前10页数据.py
@@ -1,5 +1,18 @@
 import urllib.request
 import urllib.parse
+import json
+
+# 第一页
+# https://movie.douban.com/j/chart/top_list?type=11&interval_id=100:90&action=
+# &start=0&limit=20
+# 第二页
+#
+# 第三页
+#
+# 第四页
+#
+# 第五页
+#
 url = ''
 headers = {"User-Agent" :
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"}
